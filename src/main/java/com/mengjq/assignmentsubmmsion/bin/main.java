173139dbede13374ad0eeda316f864f0e1db023b
Junqingqing <gwills@163.com>
2022-07-06 15:38:04 +0800

    develop test

---
+++ src/main/java/com/mengjq/assignmentsubmmsion/bin/main.java
@@ -0,0 +1,24 @@
+package com.mengjq.assignmentsubmmsion.bin;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Open the
+        // 主程序是
+        //
+        //双击配置信息
+        //选择1 输入学生信息 （保存至云端？识别当前设备特征为.xxx）
+        //   1 检索相似信息并保存至本地
+        //选择2 查询提交状态
+        //   输出所有数据库中提交的信息
+        //
+        //发送文件
+        //0. 获取拖拽输入
+        //1. [Record] 查询最新Assignment数据
+        //2. 展示最新Assignment数据
+        //3. [Record] 获取用户选择
+        //4. [Record] 尝试重命名文件，并提示报错 if exists
+        //5. [Record] 上交 Files
+    }
+}
