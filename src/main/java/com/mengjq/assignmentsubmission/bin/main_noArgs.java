324f577926239afceac891b154f38ac7770e05c1
Restyled.io <commits@restyled.io>
2022-10-05 09:26:49 +0000

    Restyled by clang-format

--- src/main/java/com/mengjq/assignmentsubmission/bin/main_noArgs.java
+++ src/main/java/com/mengjq/assignmentsubmission/bin/main_noArgs.java
@@ -4,70 +4,74 @@ import com.mengjq.assignmentsubmission.conf.Config;
 import com.mengjq.assignmentsubmission.conf.LanguageSet;
 import com.mengjq.assignmentsubmission.core.EchoCLI;
 import com.mengjq.assignmentsubmission.core.Menu;
-import com.mengjq.assignmentsubmission.pojo.DeviceInfo;
 import com.mengjq.assignmentsubmission.core.ServiceMainMongoDB;
+import com.mengjq.assignmentsubmission.pojo.DeviceInfo;
 import com.mengjq.assignmentsubmission.pojo.StudentInfo;
 import com.mongodb.client.FindIterable;
+import java.util.Scanner;
+import javax.print.Doc;
 import org.bson.Document;
 
-import javax.print.Doc;
-import java.util.Scanner;
-
 public class main_noArgs {
-    // 配置界面 Configure CLI
-    public static void run(StudentInfo stuInfo, EchoCLI echoCLI,
-                           ServiceMainMongoDB mongoDBService, DeviceInfo deviceInfo,
-                           Config conf, LanguageSet languageSet, boolean isRegistered) {
+  // 配置界面 Configure CLI
+  public static void run(StudentInfo stuInfo, EchoCLI echoCLI,
+                         ServiceMainMongoDB mongoDBService,
+                         DeviceInfo deviceInfo, Config conf,
+                         LanguageSet languageSet, boolean isRegistered) {
 
-        stuInfo = mongoDBService.firstRegister(deviceInfo, stuInfo, isRegistered);
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            int select = Menu.selectSettingMenu(languageSet);
-            switch (select) {
-                case 1:
-                    // 更新个人信息 - Update Personal Info
-                    assert stuInfo != null;
-                    String lastMAC = mongoDBService.getLastMAC(stuInfo.getStuId());
-                    mongoDBService.updateRecord(deviceInfo, stuInfo, lastMAC);
-                    Document stuInfoDoc = mongoDBService.updateStuInfo(); // 更新个人信息 - Update Personal Info
-                    stuInfo.updateStudent(stuInfoDoc);
-                    deviceInfo = mongoDBService.regCurrentDevice(deviceInfo, stuInfo); // 更新设备信息 - Update Device Info
-                    break;
-                case 2:
-                    if (stuInfo == null) {
-                        echoCLI.colorPrint(languageSet.setStuInfoFirst, "red");
-                        break;
-                    }
-                    // 查看自己提交的作业 - View your submitted assignments
-                    mongoDBService.requestSelfRecord(deviceInfo);
-                    FindIterable<Document> myFiles = mongoDBService.getMySubmittedFileInfo(stuInfo.getStuId());
-                    echoCLI.showMySubmitStatus(myFiles);
-                    echoCLI.manageMySubmits(myFiles, stuInfo, mongoDBService, languageSet);
-                    break;
-                case 3:
-                    // 查看所有提交的作业 - View all submitted assignments
-                    mongoDBService.requestAllRecord(deviceInfo);
-                    FindIterable<Document> assignments = mongoDBService.getCollectingAssignments();
-                    FindIterable<Document> allFiles = mongoDBService.getAllSubmittedFileInfo();
-                    FindIterable<Document> allStuInfo = mongoDBService.getAllStuInfo();
-                    echoCLI.showAllSubmitStatusBasically(allFiles);
-                    // TODO: 通过表格的形式展示所有的作业信息
-//                    echoCLI.showAllSubmitStatus(allFiles);
-                    break;
-                case 4:
-                    echoCLI.getMenuAbout(languageSet);
-                    break;
-                case 5:
-                    System.exit(0);
-                    return;
-                default:
-                    echoCLI.colorPrint(languageSet.setModeInvalidInput, "red");
-                    break;
-            }
-            echoCLI.colorPrint(languageSet.sendModeAnyKeyContinue, "green");
-            sc.nextLine();
-
-
+    stuInfo = mongoDBService.firstRegister(deviceInfo, stuInfo, isRegistered);
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      int select = Menu.selectSettingMenu(languageSet);
+      switch (select) {
+      case 1:
+        // 更新个人信息 - Update Personal Info
+        assert stuInfo != null;
+        String lastMAC = mongoDBService.getLastMAC(stuInfo.getStuId());
+        mongoDBService.updateRecord(deviceInfo, stuInfo, lastMAC);
+        Document stuInfoDoc =
+            mongoDBService
+                .updateStuInfo(); // 更新个人信息 - Update Personal Info
+        stuInfo.updateStudent(stuInfoDoc);
+        deviceInfo = mongoDBService.regCurrentDevice(
+            deviceInfo, stuInfo); // 更新设备信息 - Update Device Info
+        break;
+      case 2:
+        if (stuInfo == null) {
+          echoCLI.colorPrint(languageSet.setStuInfoFirst, "red");
+          break;
         }
+        // 查看自己提交的作业 - View your submitted assignments
+        mongoDBService.requestSelfRecord(deviceInfo);
+        FindIterable<Document> myFiles =
+            mongoDBService.getMySubmittedFileInfo(stuInfo.getStuId());
+        echoCLI.showMySubmitStatus(myFiles);
+        echoCLI.manageMySubmits(myFiles, stuInfo, mongoDBService, languageSet);
+        break;
+      case 3:
+        // 查看所有提交的作业 - View all submitted assignments
+        mongoDBService.requestAllRecord(deviceInfo);
+        FindIterable<Document> assignments =
+            mongoDBService.getCollectingAssignments();
+        FindIterable<Document> allFiles =
+            mongoDBService.getAllSubmittedFileInfo();
+        FindIterable<Document> allStuInfo = mongoDBService.getAllStuInfo();
+        echoCLI.showAllSubmitStatusBasically(allFiles);
+        // TODO: 通过表格的形式展示所有的作业信息
+        //                    echoCLI.showAllSubmitStatus(allFiles);
+        break;
+      case 4:
+        echoCLI.getMenuAbout(languageSet);
+        break;
+      case 5:
+        System.exit(0);
+        return;
+      default:
+        echoCLI.colorPrint(languageSet.setModeInvalidInput, "red");
+        break;
+      }
+      echoCLI.colorPrint(languageSet.sendModeAnyKeyContinue, "green");
+      sc.nextLine();
     }
+  }
 }
--- src/main/java/com/mengjq/assignmentsubmission/conf/LanguageSet.java
+++ src/main/java/com/mengjq/assignmentsubmission/conf/LanguageSet.java
@@ -1,149 +1,152 @@
 package com.mengjq.assignmentsubmission.conf;
 
 public class LanguageSet {
-    public String currentStudentInfo;
-    public String sendModePrompt;
-    public String setStuInfoFirst;
-    public String sendUploadOver;
-    public String menuRenameSelectPrompt;
-    public String menuPlzInputAssiId;
-    public String menuAssiIdNotExist;
-    public String selectLoopSelected;
-    public String selectLoopSelectInvalid;
-    public String menu1title;
-    public String menu2title;
-    public String menu3title;
-    public String menu4title;
-    public String menu5title;
-    public String setModeInvalidInput;
-    public String echoCLIShowAllStatusNoAssign;
-    public String echoCLIShowAllStatusNoFile;
-    public String echoCLIShowAllStatusNoStu;
-    public String echoCLIShowAllStatusUnknown;
-    public String menuAbout;
-    public String sendModeAnyKeyContinue;
-    public String sendUploadFailed;
-    public String echoCLIMySubmitsInvalidInput;
-    public String deleteSuccess;
-    public String downloadSuccess;
-    public String exitManage;
-    public String timeWait;
+  public String currentStudentInfo;
+  public String sendModePrompt;
+  public String setStuInfoFirst;
+  public String sendUploadOver;
+  public String menuRenameSelectPrompt;
+  public String menuPlzInputAssiId;
+  public String menuAssiIdNotExist;
+  public String selectLoopSelected;
+  public String selectLoopSelectInvalid;
+  public String menu1title;
+  public String menu2title;
+  public String menu3title;
+  public String menu4title;
+  public String menu5title;
+  public String setModeInvalidInput;
+  public String echoCLIShowAllStatusNoAssign;
+  public String echoCLIShowAllStatusNoFile;
+  public String echoCLIShowAllStatusNoStu;
+  public String echoCLIShowAllStatusUnknown;
+  public String menuAbout;
+  public String sendModeAnyKeyContinue;
+  public String sendUploadFailed;
+  public String echoCLIMySubmitsInvalidInput;
+  public String deleteSuccess;
+  public String downloadSuccess;
+  public String exitManage;
+  public String timeWait;
 
-    public LanguageSet(String nationCode){
+  public LanguageSet(String nationCode) {
     // I want to build a more Generic Tool, so I can help more people. :D
-        if (nationCode == null){
-            nationCode = "en";
-        }
-        switch (nationCode){
-            case "cn":
-            case "CN":
-                setWordInCN();
-                break;
-            case "en":
-            case "EN":
-            default:
-                setWordInEN();
-                break;
-        }
+    if (nationCode == null) {
+      nationCode = "en";
     }
-
-    // set all word in Chinese here
-    public void setWordInCN() {
-        currentStudentInfo = "当前学生信息";
-        sendModePrompt = "\t准备提交: ";
-        setStuInfoFirst = "请先设置学生信息";
-        sendUploadOver= "上交处理结束！";
-        sendUploadFailed = "上交失败！";
-        menuRenameSelectPrompt = "这个作业对应于:";
-        menuPlzInputAssiId = "\t请输入此作业对应的作业编号:";
-        menuAssiIdNotExist = "\t作业编号不存在，请重新输入！";
-        selectLoopSelected = "已选择:";
-        selectLoopSelectInvalid = "选择无效，请重新选择！\r";
-        menu1title = "1. 注册学生信息";
-        menu2title = "2. 管理已提交作业(云端)";
-        menu3title = "3. 查看所有人提交(云端)";
-        menu4title = "4. 关于";
-        menu5title = "5. 退出";
-        setModeInvalidInput = "无效输入!";
-        echoCLIShowAllStatusNoAssign = "没有作业！";
-        echoCLIShowAllStatusNoFile = "没有提交文件！";
-        echoCLIShowAllStatusNoStu = "未找到学生信息";
-        echoCLIShowAllStatusUnknown = "未知";
-        sendModeAnyKeyContinue = "按任意键继续";
-        echoCLIMySubmitsInvalidInput = "无效输入！\n" +
-                "删除作业: del [id(,id,id,...)]  \n" +
-                "\t例如: del 1 & del 2,5,2\n" +
-                "下载作业: down [id(,id,id,...)]  \n" +
-                "\t例如: down 1, down 2,5,2\n" +
-                "退出: exit \n";
-        deleteSuccess = "删除成功！";
-        downloadSuccess = "下载成功！";
-        exitManage = "[Enter]退出管理";
-        timeWait = "即将退出 ";
-        menuAbout = "\n" +
-                "\n" +
-                "     _            _      ____        _     \n" +
-                "    / \\   ___ ___(_)    / ___| _   _| |__  \n" +
-                "   / _ \\ / __/ __| |____\\___ \\| | | | '_ \\ \n" +
-                "  / ___ \\\\__ \\__ \\ |_____|__) | |_| | |_) |\n" +
-                " /_/   \\_\\___/___/_|    |____/ \\__,_|_.__/ \n" +
-                "" +
-                "Release Version: 1.0.0\n" +
-                "Author: Mengjq\n" +
-                "Github:\n" +
-                "Java Desktop Edition: git@github.com:GWillS163/AssignmentSubmission_Java_Edition.git\n" +
-                "Java Web Edition: ***\n" +
-                "Python Desktop Edition: ***\n" +
-                "Python Web Edition: ***";
-
+    switch (nationCode) {
+    case "cn":
+    case "CN":
+      setWordInCN();
+      break;
+    case "en":
+    case "EN":
+    default:
+      setWordInEN();
+      break;
     }
+  }
 
-    // set all word in English here
-    public void setWordInEN() {
-        currentStudentInfo = "Current Student Info";
-        sendModePrompt = "Ready to Submit";
-        setStuInfoFirst = "Please set student info first";
-        menuRenameSelectPrompt = "Which assignments fit with :";
-        sendUploadOver = "upload finished!";
-        sendUploadFailed = "upload failed!";
-        menuPlzInputAssiId = "\tPlease input the assiId you want to rename:";
-        menuAssiIdNotExist = "\tThe assiId is not exist! input again!";
-        selectLoopSelected = "Selected:";
-        selectLoopSelectInvalid = "Invalid input! input again!\r";
-        menu1title = "1. Configure personal information";
-        menu2title = "2. View submitted assignments (cloud)";
-        menu3title = "3. View all submitted (cloud)";
-        menu4title = "4. About";
-        menu5title = "5. Exit";
-        setModeInvalidInput = "Invalid input!";
-        echoCLIShowAllStatusNoAssign = "No assignments!";
-        echoCLIShowAllStatusNoFile = "No files!";
-        echoCLIShowAllStatusNoStu = "No student info found";
-        echoCLIShowAllStatusUnknown = "Unknown";
-        sendModeAnyKeyContinue = "Press any key to continue";
-        echoCLIMySubmitsInvalidInput = "无效输入！\n" +
-                "删除作业: del [id(,id,id,...)]  \n" +
-                "\t例如: del 1 & del 2,5,2\n" +
-                "下载作业: down [id(,id,id,...)]  \n" +
-                "\t例如: down 1, down 2,5,2   \n" +
-                "退出: exit \n";
-        deleteSuccess = "删除成功！";
-        downloadSuccess = "下载成功！";
-        exitManage = "[Enter]退出管理";
-        timeWait = "即将退出";
-        menuAbout = "/***\n  " +
-                "     _            _      ____        _           \n" +
-                "    / \\   ___ ___(_)    / ___| _   _| |__  \n" +
-                "   / _ \\ / __/ __| |____\\___ \\| | | | '_ \\ \n" +
-                "  / ___ \\\\__ \\__ \\ |_____|__) | |_| | |_) |\n" +
-                " /_/   \\_\\___/___/_|    |____/ \\__,_|_.__/ \n" +
-                "\n" +
-                "Release Version: 1.0.0\n" +
-                "Author: Mengjq\n" +
-                "Github:\n" +
-                "Java Desktop Edition: git@github.com:GWillS163/AssignmentSubmission_Java_Edition.git\n" +
-                "Java Web Edition: ***\n" +
-                "Python Desktop Edition: ***\n" +
-                "Python Web Edition: ***";
-    }
+  // set all word in Chinese here
+  public void setWordInCN() {
+    currentStudentInfo = "当前学生信息";
+    sendModePrompt = "\t准备提交: ";
+    setStuInfoFirst = "请先设置学生信息";
+    sendUploadOver = "上交处理结束！";
+    sendUploadFailed = "上交失败！";
+    menuRenameSelectPrompt = "这个作业对应于:";
+    menuPlzInputAssiId = "\t请输入此作业对应的作业编号:";
+    menuAssiIdNotExist = "\t作业编号不存在，请重新输入！";
+    selectLoopSelected = "已选择:";
+    selectLoopSelectInvalid = "选择无效，请重新选择！\r";
+    menu1title = "1. 注册学生信息";
+    menu2title = "2. 管理已提交作业(云端)";
+    menu3title = "3. 查看所有人提交(云端)";
+    menu4title = "4. 关于";
+    menu5title = "5. 退出";
+    setModeInvalidInput = "无效输入!";
+    echoCLIShowAllStatusNoAssign = "没有作业！";
+    echoCLIShowAllStatusNoFile = "没有提交文件！";
+    echoCLIShowAllStatusNoStu = "未找到学生信息";
+    echoCLIShowAllStatusUnknown = "未知";
+    sendModeAnyKeyContinue = "按任意键继续";
+    echoCLIMySubmitsInvalidInput = "无效输入！\n"
+                                   + "删除作业: del [id(,id,id,...)]  \n"
+                                   + "\t例如: del 1 & del 2,5,2\n"
+                                   + "下载作业: down [id(,id,id,...)]  \n"
+                                   + "\t例如: down 1, down 2,5,2\n"
+                                   + "退出: exit \n";
+    deleteSuccess = "删除成功！";
+    downloadSuccess = "下载成功！";
+    exitManage = "[Enter]退出管理";
+    timeWait = "即将退出 ";
+    menuAbout =
+        "\n"
+        + "\n"
+        + "     _            _      ____        _     \n"
+        + "    / \\   ___ ___(_)    / ___| _   _| |__  \n"
+        + "   / _ \\ / __/ __| |____\\___ \\| | | | '_ \\ \n"
+        + "  / ___ \\\\__ \\__ \\ |_____|__) | |_| | |_) |\n"
+        + " /_/   \\_\\___/___/_|    |____/ \\__,_|_.__/ \n"
+        + ""
+        + "Release Version: 1.0.0\n"
+        + "Author: Mengjq\n"
+        + "Github:\n"
+        +
+        "Java Desktop Edition: git@github.com:GWillS163/AssignmentSubmission_Java_Edition.git\n"
+        + "Java Web Edition: ***\n"
+        + "Python Desktop Edition: ***\n"
+        + "Python Web Edition: ***";
+  }
+
+  // set all word in English here
+  public void setWordInEN() {
+    currentStudentInfo = "Current Student Info";
+    sendModePrompt = "Ready to Submit";
+    setStuInfoFirst = "Please set student info first";
+    menuRenameSelectPrompt = "Which assignments fit with :";
+    sendUploadOver = "upload finished!";
+    sendUploadFailed = "upload failed!";
+    menuPlzInputAssiId = "\tPlease input the assiId you want to rename:";
+    menuAssiIdNotExist = "\tThe assiId is not exist! input again!";
+    selectLoopSelected = "Selected:";
+    selectLoopSelectInvalid = "Invalid input! input again!\r";
+    menu1title = "1. Configure personal information";
+    menu2title = "2. View submitted assignments (cloud)";
+    menu3title = "3. View all submitted (cloud)";
+    menu4title = "4. About";
+    menu5title = "5. Exit";
+    setModeInvalidInput = "Invalid input!";
+    echoCLIShowAllStatusNoAssign = "No assignments!";
+    echoCLIShowAllStatusNoFile = "No files!";
+    echoCLIShowAllStatusNoStu = "No student info found";
+    echoCLIShowAllStatusUnknown = "Unknown";
+    sendModeAnyKeyContinue = "Press any key to continue";
+    echoCLIMySubmitsInvalidInput = "无效输入！\n"
+                                   + "删除作业: del [id(,id,id,...)]  \n"
+                                   + "\t例如: del 1 & del 2,5,2\n"
+                                   + "下载作业: down [id(,id,id,...)]  \n"
+                                   + "\t例如: down 1, down 2,5,2   \n"
+                                   + "退出: exit \n";
+    deleteSuccess = "删除成功！";
+    downloadSuccess = "下载成功！";
+    exitManage = "[Enter]退出管理";
+    timeWait = "即将退出";
+    menuAbout =
+        "/***\n  "
+        + "     _            _      ____        _           \n"
+        + "    / \\   ___ ___(_)    / ___| _   _| |__  \n"
+        + "   / _ \\ / __/ __| |____\\___ \\| | | | '_ \\ \n"
+        + "  / ___ \\\\__ \\__ \\ |_____|__) | |_| | |_) |\n"
+        + " /_/   \\_\\___/___/_|    |____/ \\__,_|_.__/ \n"
+        + "\n"
+        + "Release Version: 1.0.0\n"
+        + "Author: Mengjq\n"
+        + "Github:\n"
+        +
+        "Java Desktop Edition: git@github.com:GWillS163/AssignmentSubmission_Java_Edition.git\n"
+        + "Java Web Edition: ***\n"
+        + "Python Desktop Edition: ***\n"
+        + "Python Web Edition: ***";
+  }
 }
